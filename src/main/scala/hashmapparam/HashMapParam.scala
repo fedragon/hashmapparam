6f27fef5ab00ad21e0ef23738d08943d7b605a9c
mkerry <mkerry@millennialmedia.com>
2015-07-08 00:20:08 -0400

    Made HashMap params generic to allow any key type and any Numeric value.

--- src/main/scala/hashmapparam/HashMapParam.scala
+++ src/main/scala/hashmapparam/HashMapParam.scala
@@ -3,18 +3,19 @@ package hashmapparam
 import org.apache.spark.{AccumulableParam, SparkConf}
 import org.apache.spark.serializer.JavaSerializer
 import scala.collection.mutable.{HashMap => MutableHashMap}
+import Numeric.Implicits._
 
 /*
- * Allows a mutable HashMap[String, Int] to be used as an accumulator in Spark.
+ * Allows a mutable HashMap[A, Numeric] to be used as an accumulator in Spark.
  * Whenever we try to put (k, v2) into an accumulator that already contains (k, v1), the result
  * will be a HashMap containing (k, v1 + v2).
  *
  * Would have been nice to extend GrowableAccumulableParam instead of redefining everything, but it's
  * private to the spark package.
  */
-class HashMapParam extends AccumulableParam[MutableHashMap[String, Int], (String, Int)] {
+class HashMapParam[A, B: Numeric] extends AccumulableParam[MutableHashMap[A, B], (A, B)] {
 
-  def addAccumulator(acc: MutableHashMap[String, Int], elem: (String, Int)): MutableHashMap[String, Int] = {
+  def addAccumulator(acc: MutableHashMap[A, B], elem: (A, B)): MutableHashMap[A, B] = {
     val (k1, v1) = elem
     acc += acc.find(_._1 == k1).map {
       case (k2, v2) => k2 -> (v1 + v2)
@@ -28,7 +29,7 @@ class HashMapParam extends AccumulableParam[MutableHashMap[String, Int], (String
    *
    * @see org.apache.spark.GrowableAccumulableParam.addInPlace(r1: R, r2: R): R
    */
-  def addInPlace(acc1: MutableHashMap[String, Int], acc2: MutableHashMap[String, Int]): MutableHashMap[String, Int] = {
+  def addInPlace(acc1: MutableHashMap[A, B], acc2: MutableHashMap[A, B]): MutableHashMap[A, B] = {
     acc2.foreach(elem => addAccumulator(acc1, elem))
     acc1
   }
@@ -36,9 +37,9 @@ class HashMapParam extends AccumulableParam[MutableHashMap[String, Int], (String
   /*
    * @see org.apache.spark.GrowableAccumulableParam.zero(initialValue: R): R
    */
-  def zero(initialValue: MutableHashMap[String, Int]): MutableHashMap[String, Int] = {
+  def zero(initialValue: MutableHashMap[A, B]): MutableHashMap[A, B] = {
     val ser = new JavaSerializer(new SparkConf(false)).newInstance()
-    val copy = ser.deserialize[MutableHashMap[String, Int]](ser.serialize(initialValue))
+    val copy = ser.deserialize[MutableHashMap[A, B]](ser.serialize(initialValue))
     copy.clear()
     copy
   }
